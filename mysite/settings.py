1eef37041a2a8154d535fb98a20cee7bfc8e7588
LevNut <teeranut.s@ku.th>
2020-09-12 13:02:19 +0700

    delete requirement.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import environ
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+env = environ.Env()
+env.read_env()
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6!!-)!!j5ndutc2#6h%icvp(603hmdvvpbd0pbnc9#vas*b8t4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
